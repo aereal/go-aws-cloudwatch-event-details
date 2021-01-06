faf57740a01c888d2b9701da4434295d42bf4929
aereal <aereal@aereal.org>
2021-01-06 18:16:19 +0900

    test: extract expectation

--- parse_test.go
+++ parse_test.go
@@ -18,70 +18,7 @@ func TestParseEventDetail(t *testing.T) {
 		want         interface{}
 		wantErr      bool
 	}{
-		{"ECS/ok", "testdata/ecs.ok.json", &ECSTaskStateChangeEvent{
-			Attachments: []TaskAttachment{
-				{
-					ID:     "1789bcae-ddfb-4d10-8ebe-8ac87ddba5b8",
-					Type:   "eni",
-					Status: "ATTACHED",
-					Details: []TaskAttachmentDetail{
-						{
-							Name:  "subnetId",
-							Value: "subnet-abcd1234",
-						},
-						{
-							Name:  "networkInterfaceId",
-							Value: "eni-abcd1234",
-						},
-						{
-							Name:  "macAddress",
-							Value: "0a:98:eb:a7:29:ba",
-						},
-						{
-							Name:  "privateIPv4Address",
-							Value: "10.0.0.139",
-						},
-					},
-				},
-			},
-			AvailabilityZone: "us-west-2c",
-			ClusterArn:       "arn:aws:ecs:us-west-2:111122223333:cluster/FargateCluster",
-			Containers: []Container{
-				{
-					ContainerArn: "arn:aws:ecs:us-west-2:111122223333:container/cf159fd6-3e3f-4a9e-84f9-66cbe726af01",
-					LastStatus:   "RUNNING",
-					Name:         "FargateApp",
-					Image:        "111122223333.dkr.ecr.us-west-2.amazonaws.com/hello-repository:latest",
-					ImageDigest:  "sha256:74b2c688c700ec95a93e478cdb959737c148df3fbf5ea706abe0318726e885e6",
-					RuntimeID:    "ad64cbc71c7fb31c55507ec24c9f77947132b03d48d9961115cf24f3b7307e1e",
-					TaskArn:      "arn:aws:ecs:us-west-2:111122223333:task/FargateCluster/c13b4cb40f1f4fe4a2971f76ae5a47ad",
-					NetworkInterfaces: []NetworkInterface{
-						{
-							AttachmentID:       "1789bcae-ddfb-4d10-8ebe-8ac87ddba5b8",
-							PrivateIpv4Address: "10.0.0.139",
-						},
-					},
-					CPU: "0",
-				},
-			},
-			LaunchType:        "FARGATE",
-			CPU:               "256",
-			Memory:            "512",
-			DesiredStatus:     "RUNNING",
-			Group:             "family:sample-fargate",
-			LastStatus:        "RUNNING",
-			Connectivity:      "CONNECTED",
-			TaskArn:           "arn:aws:ecs:us-west-2:111122223333:task/FargateCluster/c13b4cb40f1f4fe4a2971f76ae5a47ad",
-			TaskDefinitionArn: "arn:aws:ecs:us-west-2:111122223333:task-definition/sample-fargate:1",
-			Version:           4,
-			PlatformVersion:   "1.3.0",
-			ConnectivityAt:    mustParseTime("2020-01-23T17:57:38.453Z"),
-			PullStartedAt:     mustParseTime("2020-01-23T17:57:52.103Z"),
-			PullStoppedAt:     mustParseTime("2020-01-23T17:57:55.103Z"),
-			StartedAt:         mustParseTime("2020-01-23T17:57:58.103Z"),
-			CreatedAt:         mustParseTime("2020-01-23T17:57:34.402Z"),
-			UpdatedAt:         mustParseTime("2020-01-23T17:57:58.103Z"),
-		}, false},
+		{"ECS/ok", "testdata/ecs.ok.json", ecsTaskStateChangeOK, false},
 	}
 	cwd, err := os.Getwd()
 	if err != nil {
@@ -120,3 +57,70 @@ func mustParseTime(repr string) time.Time {
 	}
 	return parsed
 }
+
+var (
+	ecsTaskStateChangeOK = &ECSTaskStateChangeEvent{
+		Attachments: []TaskAttachment{
+			{
+				ID:     "1789bcae-ddfb-4d10-8ebe-8ac87ddba5b8",
+				Type:   "eni",
+				Status: "ATTACHED",
+				Details: []TaskAttachmentDetail{
+					{
+						Name:  "subnetId",
+						Value: "subnet-abcd1234",
+					},
+					{
+						Name:  "networkInterfaceId",
+						Value: "eni-abcd1234",
+					},
+					{
+						Name:  "macAddress",
+						Value: "0a:98:eb:a7:29:ba",
+					},
+					{
+						Name:  "privateIPv4Address",
+						Value: "10.0.0.139",
+					},
+				},
+			},
+		},
+		AvailabilityZone: "us-west-2c",
+		ClusterArn:       "arn:aws:ecs:us-west-2:111122223333:cluster/FargateCluster",
+		Containers: []Container{
+			{
+				ContainerArn: "arn:aws:ecs:us-west-2:111122223333:container/cf159fd6-3e3f-4a9e-84f9-66cbe726af01",
+				LastStatus:   "RUNNING",
+				Name:         "FargateApp",
+				Image:        "111122223333.dkr.ecr.us-west-2.amazonaws.com/hello-repository:latest",
+				ImageDigest:  "sha256:74b2c688c700ec95a93e478cdb959737c148df3fbf5ea706abe0318726e885e6",
+				RuntimeID:    "ad64cbc71c7fb31c55507ec24c9f77947132b03d48d9961115cf24f3b7307e1e",
+				TaskArn:      "arn:aws:ecs:us-west-2:111122223333:task/FargateCluster/c13b4cb40f1f4fe4a2971f76ae5a47ad",
+				NetworkInterfaces: []NetworkInterface{
+					{
+						AttachmentID:       "1789bcae-ddfb-4d10-8ebe-8ac87ddba5b8",
+						PrivateIpv4Address: "10.0.0.139",
+					},
+				},
+				CPU: "0",
+			},
+		},
+		LaunchType:        "FARGATE",
+		CPU:               "256",
+		Memory:            "512",
+		DesiredStatus:     "RUNNING",
+		Group:             "family:sample-fargate",
+		LastStatus:        "RUNNING",
+		Connectivity:      "CONNECTED",
+		TaskArn:           "arn:aws:ecs:us-west-2:111122223333:task/FargateCluster/c13b4cb40f1f4fe4a2971f76ae5a47ad",
+		TaskDefinitionArn: "arn:aws:ecs:us-west-2:111122223333:task-definition/sample-fargate:1",
+		Version:           4,
+		PlatformVersion:   "1.3.0",
+		ConnectivityAt:    mustParseTime("2020-01-23T17:57:38.453Z"),
+		PullStartedAt:     mustParseTime("2020-01-23T17:57:52.103Z"),
+		PullStoppedAt:     mustParseTime("2020-01-23T17:57:55.103Z"),
+		StartedAt:         mustParseTime("2020-01-23T17:57:58.103Z"),
+		CreatedAt:         mustParseTime("2020-01-23T17:57:34.402Z"),
+		UpdatedAt:         mustParseTime("2020-01-23T17:57:58.103Z"),
+	}
+)
